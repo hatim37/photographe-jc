42b7cf2646c3fef4d7e87ff0a555b38ef9db3095
Hatim m <hatim860@gmail.com>
2022-12-25 14:26:08 +0100

    ajout gallerie, navbar, article

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('JavaScript');
+    
     
 
 };
---
+++ posts/grossesse/mon-2eme-article.md
@@ -0,0 +1,7 @@
+---
+title: My first post
+description: The first post on the Eleventy + Netlify CMS from scratch blog
+featuredImage: /images/uploads/Dubai1.jpg
+---
+
+Voici un texte p
