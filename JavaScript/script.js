38de84cf33a8465f176bfed9131dd45a31fc58bc
Hatim m <hatim.web.dev@gmail.com>
2023-05-09 14:15:40 +0200

    Modif lien js libs

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,35 @@
-       
-        $('.portfolio-menu ul li').click(function(){
-            $('.portfolio-menu ul li').removeClass('active');
-            $(this).addClass('active');
-            
-            let selector = $(this).attr('data-filter');
-            $('.portfolio-item').isotope({
-                filter:selector
-            });
-            return  false;
-        });
-        $(document).ready(function() {
-        let popup_btn = $('.popup-btn');
-        popup_btn.magnificPopup({
-        type : 'image',
-        gallery : {
-            enabled : true
+
+/** Filtre dynamique galerie photo */
+
+$('.portfolio-menu ul li').click(function () {
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');
+
+    let selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+        filter: selector
+    });
+    return false;
+});
+$(document).ready(function () {
+    let popup_btn = $('.popup-btn');
+    popup_btn.magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
         }
+    });
+});
+
+
+/** identification role admin netlify */
+
+if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
         });
-        });
+      }
+    });
+  }
